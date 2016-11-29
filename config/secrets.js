e654e01ebf43d738b8eff304abb98dcdbe6f3157
Yicheng Gong <ygongdev@gmail.com>
2016-11-29 13:34:18 +0000

    hid credentials

--- config/secrets.js
+++
@@ -1,7 +0,0 @@
-/*
- * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
- */
-module.exports = {
-    token : "secret-llama",
-    mongo_connection : "mongodb://localhost/mp4"
-};
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-/*
- * Connect all of your endpoints together here.
- */
-module.exports = function (app, router) {
-  app.use('/api', require('./home.js')(router));
-  app.use('/api', require('./llama.js')(router));
-};
