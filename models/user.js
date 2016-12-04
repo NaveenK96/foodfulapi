190d592ebb3195a54f21345dbb34dcb475b532a2
Yicheng Gong <ygongdev@gmail.com>
2016-12-03 19:37:29 -0600

    testing

--- models/user.js
+++ models/user.js
@@ -7,12 +7,12 @@ var jwt = require('jsonwebtoken');
 
 // Define our User Schema
 /* TypeID */
-/* 0 - Individual Donor
-   1 - Restaurant Donor
-   2 - Donatee
+/* 
+   0 - Donor
+   1 - Donatee
 */ 
 var UserSchema = new mongoose.Schema({
-  typeID: { type: Number, required: true },
+  typeID: { type: Number, required: true }, // Refer to above
   name: { type: String, required: true },
   hash: String,
   salt: String,
@@ -20,12 +20,17 @@ var UserSchema = new mongoose.Schema({
   email: { type: String, uinique: true, required: true },
   phone_number: { type: Number, required: true },
   location: { type: [Number], index: '2dsphere', required: true }, // [Long, Lat], index important for proximity searches
-  hour: { type: Date, required: true},
+  /* Hour Format HH:MM -> HH:MM */
+  start_hour: { type: Number, required: true },
+  start_minute: { type: Number, required: true },
+  end_hour: { type: Number, required: true },
+  end_minute: { type: Number, required: true },
   amount: { type: Number, default: 0 }, 
   rating: { type: Number, default: 0 }, 
+  favorites: { type: [String] }, // List of bookmarked ids  
   created_date: { type: Date, default: Date.now },
   updated_date: { type: Date, default: Date.now },
-})
+});
 
 //UserSchema.index({location: '2dsphere'});
 // User authentication and security
